5cdfb997d53e0c2379e0ff11d5191433a9b24726
Prajwal855 <98931921+Prajwal855@users.noreply.github.com>
2023-05-30 16:41:03 +0530

    Ruby Practice Code 30/05

---
+++ untitled/higest_number.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+# Programme to find Highest Number in an array
+array_of_number = [3, 1, 4, 2, 6, 9, 4, 7]
+
+sorted_array = array_of_number.sort
+
+puts("Highest Number in the array is #{sorted_array[-1]}")
+
